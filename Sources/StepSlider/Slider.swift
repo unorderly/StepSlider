bec2cde68f5adf88d2a383eccaa93f6194bdbfc9
Jake Nelson <jake@unorderly.io>
2025-09-12 19:55:22 +1000

    Set .capsule content shape for hover effect

--- Sources/StepSlider/Slider.swift
+++ Sources/StepSlider/Slider.swift
@@ -136,6 +136,7 @@ struct Slider<Value: Hashable, TrackLabel: View, ThumbLabel: View>: View {
             .background(self.trackSelectionColor
                 .clipShape(.capsule)
                 .padding(self.dragState != nil && !self.accessibilityReduceMotion ? -4 : 0))
+            .contentShape(.hoverEffect, .capsule)
             .shadow(color: Color.black.opacity(0.12), radius: 4)
             .hoverEffect(.highlight)
             .frame(width: self.values.elementWidth(in: proxy.size.width))
